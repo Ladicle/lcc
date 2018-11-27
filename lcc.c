cbe6de98e02bd7954620cbf0d1a32e2fd21be380
Aya Igarashi <ladiclexxx@gmail.com>
2018-11-27 09:36:50 +0900

    Impl stack-machine

--- lcc.c
+++ lcc.c
@@ -37,6 +37,7 @@ Node *mul();
 Node *expr();
 void tokenize(char *p);
 void error();
+void gen(Node *node);
 
 Node *new_node(int ty, Node *lhs, Node *rhs) {
   Node *node = malloc(sizeof(Node));
@@ -84,6 +85,36 @@ Node *expr() {
     return new_node(tokens[pos].ty, lhs, expr());
   }
   return lhs;
+}
+
+void gen(Node *node) {
+  if (node->ty == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->ty) {
+  case '+':
+    printf("  add rax, rdi\n");
+    break;
+  case '-':
+    printf("  sub rax, rdi\n");
+    break;
+  case '*':
+    printf("  mul rdi\n");
+    break;
+  case '/':
+    printf("  mov rdx, 0\n");
+    printf("  div rdi\n");
+  }
+
+  printf("  push rax\n");
 }
 
 // divide string argument into the tokens
